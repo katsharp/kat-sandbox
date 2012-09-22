2d2af9c3203b9c3f9eb4b178a81770f6530817d6
katsharp <katsharp@katsharp-laptop>
2012-09-22 19:04:27 +0100

    Hello world

---
+++ org.katsharp.git.test/src/org/katsharp/git/test/builder/GitTest.java
@@ -0,0 +1,12 @@
+package org.katsharp.git.test.builder;
+
+public class GitTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, git world!");
+	}
+
+}
